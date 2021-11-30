e787e7a41b09b893dee7ba76521f1d08a9fec5e2
chen-grace-p4 <gchen20@stuy.edu>
2021-11-29 22:47:31 -0500

    added function headers

--- redirection.c
+++ redirection.c
@@ -11,6 +11,11 @@
 
 #include "read.h"
 
+/*
+Performs redirection from stdout to a file
+@parameter args is the parsed arguments (a command) from the user input
+@parameter numInputs is the number of inputs - 1 since it's based on spaces counted
+*/
 void redirect_stdout_to(char ** args, int numInputs) {
    int file = open(args[numInputs], O_CREAT | O_WRONLY | O_TRUNC, 0640);
    args[numInputs-1] = NULL;
@@ -18,6 +23,11 @@ void redirect_stdout_to(char ** args, int numInputs) {
    execvp(args[0], args);
 }
 
+/*
+Performs redirection stdin from a file
+@parameter args is the parsed arguments (a command) from the user input
+@parameter numInputs is the number of inputs - 1 since it's based on spaces counted
+*/
 void redirect_stdin_from(char ** args, int numInputs) {
    int file = open(args[numInputs], O_RDONLY);
    args[numInputs-1] = NULL;
